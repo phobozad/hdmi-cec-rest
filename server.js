5eadb3d7be26b10843fb0592ddc0de0810163102
Chris Burger <phobozad@gmail.com>
2019-11-30 16:36:57 +0000

    Added device scanning, fixed rawcmd endpoint, volume control, support power on/off for other device types

--- server.js
+++ server.js
@@ -30,7 +30,21 @@ var portMapping =  {
 	13: 0x1110,
 	14: 0x1120,
 	15: 0x1130,
-	16: 0x1144 };
+	16: 0x1144,
+	31: 0x3100,
+	32: 0x3200,
+	33: 0x3300,
+	34: 0x3400,
+	35: 0x3500,
+};
+
+
+
+// Device Info Array initialization
+var deviceInfo = []
+for (i = 0x0; i < 0xF; i++) {
+	deviceInfo[i]={name: null, physicalAddress: null}
+}
 
 
 // Application API Variables
@@ -50,6 +64,18 @@ function HdmiPortMap(inputPort) {
 };
 
 
+function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function volumeUpPress() {
+	cec.sendCommand( 0xE5, CEC.Opcode.USER_CONTROL_PRESSED, CEC.UserControlCode.VOLUME_UP);
+}
+
+function volumeDownPress() {
+	cec.sendCommand( 0xE5, CEC.Opcode.USER_CONTROL_PRESSED, CEC.UserControlCode.VOLUME_DOWN);
+}
 
 // Kill cec-client process on exit
 
@@ -67,6 +93,8 @@ cec.once("ready", function(client) {
 	console.log(" -- CEC-CLIENT READY -- ");
 	cec.sendCommand( 0xE0, CEC.Opcode.GIVE_DEVICE_POWER_STATUS);
 	cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+	// Scan entire bus to discover devices
+	cec.send("scan");
 });
 
 cec.on("REPORT_POWER_STATUS", function (packet, status) {
@@ -82,14 +110,13 @@ cec.on("REPORT_POWER_STATUS", function (packet, status) {
 	}
 	for (var i = keys.length - 1; i>=0; i--) {
 		if(CEC.PowerStatus[keys[i]] == status) {
-			console.log("POWER_STATUS: ", keys[i]);
+			console.log("Logical Device: 0x" + packet.source + " POWER_STATUS: ", keys[i]);
 			break;
 		}
 	}
 });
 
 cec.on("REPORT_AUDIO_STATUS", function (packet, audioStatus) {
-	
 	// If value greater than 0x80, we are muted
 	if (audioStatus>127)
 	{
@@ -103,6 +130,7 @@ cec.on("REPORT_AUDIO_STATUS", function (packet, audioStatus) {
 		hdmiMute = false;
 		hdmiVolume = audioStatus;
 	}	
+	console.log("REPORT_AUDIO_STATUS Muted: " + hdmiMute + "  Volume: " + hdmiVolume);	
 });
 
 cec.on("ROUTING_CHANGE", function(packet, fromSource, toSource) {
@@ -115,12 +143,25 @@ cec.on("ROUTING_CHANGE", function(packet, fromSource, toSource) {
 });
 
 
+cec.on("SET_OSD_NAME", function (packet, osdname) {
+	console.log("Logical Device: 0x" + packet.source + " OSD Name: " + osdname);
+	deviceInfo[parseInt(packet.source,16)].name = osdname;
+});
+
+cec.on("REPORT_PHYSICAL_ADDRESS", function (packet, physicalAddress, logicalDest) {
+	console.log("Logical Device: 0x" + packet.source + " Physical Address: 0x" + physicalAddress.toString(16));
+	deviceInfo[parseInt(packet.source,16)].physicalAddress = parseInt("0x" + physicalAddress.toString(16),16);
+});
+
+
 // START CEC CLIENT
 
 // -m  = start in monitor-mode
 // -d8 = set log level to 8 (=TRAFFIC) (-d 8)
 // -br = logical address set to 'recording device'
 cec.start("cec-client", "-m", "-d", "8", "-t", "r");
+
+
 
 
 
@@ -178,19 +219,104 @@ router.route("/input")
 
 
 // Handle Volume
-router.route("/volume")
+router.route("/volume/:vol?")
 	.get(function(req, res) {
 		cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
-		setTimeout(function() { res.json({currentVolume: hdmiVolume}) }, 500);
+		//setTimeout(function() { res.json({currentVolume: hdmiVolume}) }, 500);
+		setTimeout(function() { res.json(hdmiVolume) }, 500);
 	})
-	.put(function(req,res) {
-		var newVolume = req.params.volume;
+	.put(async function(req,res) {
 		// Need to determine if current volume is same as 
 		// requested volume.  Can we rely on internal vol status?
 		
 		// Do some for loop to press vol up or vol down
 		// however many times to get to the desired level
-		res.send("STUB");
+		//
+		var newVolume = req.params.vol;
+		function volUpdated() {
+			return new Promise(function (resolve, reject){
+				cec.once("REPORT_AUDIO_STATUS", function (packet, audioStatus) {
+					resolve();
+				});
+			});
+		}
+		
+		cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+		var volPromise = volUpdated();
+		await volPromise.then();
+		
+		if (newVolume >=0 && newVolume <=100) {
+			
+			// Prevent endless loop trying to hit target volume
+			var attempts = 0;
+			var volDiff = 0;
+
+			while ((Math.abs(hdmiVolume-newVolume) >1) && (attempts <=10)) {
+				//console.log("Current Volume: " + hdmiVolume + "  Target Volume: " + newVolume);
+				volDiff = Math.abs(newVolume-hdmiVolume);
+
+				// Big steps if we are far away
+				if (volDiff > 10) {
+					for ( var i=0; i<Math.round(volDiff/2.5); i++) {
+						if (hdmiVolume < newVolume) {
+							volumeUpPress();
+						}
+						else {
+							volumeDownPress();
+						}
+					}
+					var lastVol = 128;
+					
+					// Wait for reported volume level to settle down before moving on
+					// there is a delay in processing the volume button commands and the actual volume state catching up
+					// Could be improved somewhat by waiting for two identical results in a row
+					while(lastVol != hdmiVolume) {
+						lastVol = hdmiVolume
+						cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+						volPromise = volUpdated();
+						await volPromise.then();
+					}
+				}
+				// Small steps once we are close
+				else {
+					if (hdmiVolume < newVolume) {
+						volumeUpPress();
+					}
+					else {
+						volumeDownPress();
+					}
+					await sleep(200);
+				}
+				
+				// Wait for updated volume level before pressing another button so we know how close/far we actually are from target
+				// Could be improved by waiting until the response comes back with different value than last time.  sometimes it seems like 
+				// reported volume isn't updated in realtime from the Amp - this can lead to overshooting the target and oscillating
+				cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+				volPromise = volUpdated();
+				await volPromise.then();
+
+				attempts++;
+			}
+			
+			await sleep(300);
+			cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+			volPromise = volUpdated();
+			await volPromise.then();
+
+			if (Math.abs(hdmiVolume-newVolume) > 2) {
+				if (hdmiVolume < newVolume) {
+					volumeUpPress();
+				}
+				else {
+					volumeDownPress();
+				}
+			}
+
+			cec.sendCommand( 0xE5, CEC.Opcode.GIVE_AUDIO_STATUS);
+
+
+		}
+		res.status(204).end();
 	});
 
 // Handle muting
@@ -247,6 +373,8 @@ router.route("/power/:device")
 	})
 	.put(function(req,res) {
 		var destinationDevice = -1;
+
+		// Check for logical device name
 		switch (req.params.device){
 			case "TV":
 				destinationDevice=CEC.LogicalAddress.TV;
@@ -254,8 +382,19 @@ router.route("/power/:device")
 			case "AUDIOSYSTEM":
 				destinationDevice=CEC.LogicalAddress.AUDIOSYSTEM;
 				break;
-			default:
-				req.status(400).send("Invalid Device");
+			//default:
+			//	req.status(400).send("Invalid Device");
+		}
+
+		// If we got a hex-looking value, assume this is a physical device address
+		if (req.params.device.startsWith("0x")) {
+			// Convert hex string to integer value
+			devicePhysical = parseInt(req.params.device,16);
+			// Make sure value is a valid physical device destination
+			if (devicePhysical >= 0x0000 && devicePhysical <= 0xFFFF) {
+				// Lookup logical device based on physical address in our device info array
+				destinationDevice = deviceInfo.findIndex(device => device.physicalAddress === devicePhysical);
+			}
 		}
 		if (destinationDevice != -1){
 			switch (req.body.state) {
@@ -265,11 +404,20 @@ router.route("/power/:device")
 					res.status(204).end();
 					break;
 				case "on":
-					cec.sendCommand((0xE0+destinationDevice), CEC.Opcode.IMAGE_VIEW_ON);
+					// TV (device 0) is a special case
+					if (destinationDevice==0) {
+						cec.sendCommand((0xE0+destinationDevice), CEC.Opcode.IMAGE_VIEW_ON);
+					}
+					else {
+						cec.sendCommand( (0xE0 + destinationDevice), CEC.Opcode.USER_CONTROL_PRESSED, CEC.UserControlCode.POWER_ON_FUNCTION);
+					}
 					console.log("Powering on %s", req.params.device);
 					res.status(204).end();
 					break;
 			}
+		}
+		else {
+			res.status(400).send("Invalid Device");
 		}
 	})
 	.delete(function(req,res) {
@@ -335,7 +483,8 @@ router.route("/audio")
 // Allow sending a raw HDMI CEC command (hex)
 router.route("/rawcmd/:cmd")
 	.put(function(req, res) {
-		cec.sendCommand( 0xE5, req.params.cmd);
+		cec.send( "tx " + req.params.cmd);
+		res.status(204).end();
 	});
 
 router.route("/wol/:mac")
@@ -349,6 +498,26 @@ router.route("/wol/:mac")
 			}
 		});
 	});
+
+
+router.route("/rescan")
+	.get(function (req, res) {
+		cec.send("scan");
+		res.status(204).end();
+	});
+
+router.route("/devicelist")
+	.get(function (req, res) {
+
+		var deviceList = JSON.stringify(deviceInfo, (key, value) => {
+			  if( typeof value === 'number'){
+				      return '0x' + value.toString(16)
+				    }
+			  return value
+		},4);
+		res.status(200).send(deviceList);
+	});
+
 // more routes can go here
 
 // REGISTER OUR ROUTES -------------
